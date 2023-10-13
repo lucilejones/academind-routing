41ac2470d39b7de7d7bbfc0d46108c3f1c1c1160
Lucile Jones <jones.lucile@gmail.com>
2023-10-13 12:53:40 -0600

    working on params

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,9 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServers(id: number) {
     // complex calculation, reach out to backend, then want to navigate away
-    this.router.navigate(['/servers']);
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 
 }
